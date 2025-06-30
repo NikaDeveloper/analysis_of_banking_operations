cf1543ff9a810594a23eda77a2bda28112ccf334
Nika Alexandrovna <megrelynika@gmail.com>
2025-06-30 20:46:41 +0400

    Organization of the project.

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from src.utils import read_excel_data
+
+excel_file_path = "../data/operations.xlsx"
+
+
+operations_df = read_excel_data(excel_file_path)
+
+
+if not operations_df.empty:
+    print("Данные успешно прочитаны:")
+    print(operations_df.head())
+else:
+    print("Не удалось прочитать данные из Excel файла.")
